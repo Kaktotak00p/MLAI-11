a1757f2fc4df6138c01b63b1e3232c85811020ac
Lipiński, Kamil (Stud. DACS) <k.lipinski@student.maastrichtuniversity.nl>
2024-11-18 22:32:35 +0100

    Started working on the sound

---
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/BallColllisionSound.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BallCollisionSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+}
